080b559fe9a093b02276f280a112097d8c8796a6
Rick Russotto <russotto@ldeo.columbia.edu>
2019-01-07 11:39:50 -0500

    Initial commit
    I believe this is ready for release so I am going to put it on Github.
    In "plots" folder are pdf and png hurricane season timeline plots for
    each year from 1851 through 2017, and a color legend.
    In "data" folder is the NOAA HURDAT2 Atlantic database, and the same
    data split up into individual files for each storm in directories for
    each year.
    In top directory are iPython notebooks to split up the dataset into
    individual files, make and save the plots, and define retired storm
    names (these are bold in the plots).

---
+++ RetiredNames.py
@@ -0,0 +1,91 @@
+
+# coding: utf-8
+
+# In[4]:
+
+#List of retired storm names, for bolding in the "plotAllSeasons" notebook.
+#Dictionary, with key being year (int) and list of names in each year.
+#Will need to be saved as a python script so it can be imported.
+
+#For Pacific, include Central Pacific 
+#retired names as these are included in HURDAT2. 
+
+
+# In[5]:
+
+Atlantic = {1954: ['Carol', 'Edna', 'Hazel'],
+            1955: ['Connie', 'Diane', 'Ione', 'Janet'],
+            1957: ['Audrey'], 
+            1960: ['Donna'], 
+            1961: ['Carla', 'Hattie'], 
+            1963: ['Flora'], 
+            1964: ['Cleo', 'Dora', 'Hilda'], 
+            1965: ['Betsy'], 
+            1966: ['Inez'], 
+            1967: ['Beulah'], 
+            1969: ['Camille'], 
+            1970: ['Celia'], 
+            1972: ['Agnes'], 
+            1974: ['Carmen', 'Fifi'], 
+            1975: ['Eloise'], 
+            1977: ['Anita'], 
+            1978: ['Greta'], 
+            1979: ['David', 'Frederic'], 
+            1980: ['Allen'], 
+            1983: ['Alicia'], 
+            1985: ['Elena', 'Gloria'], 
+            1988: ['Gilbert', 'Joan'], 
+            1989: ['Hugo'], 
+            1990: ['Diana', 'Klaus'], 
+            1991: ['Bob'], 
+            1992: ['Andrew'], 
+            1995: ['Luis', 'Marilyn', 'Opal', 'Roxanne'], 
+            1996: ['Cesar', 'Fran', 'Hortense'], 
+            1998: ['Georges', 'Mitch'], 
+            1999: ['Floyd', 'Lenny'], 
+            2000: ['Keith'], 
+            2001: ['Allison', 'Iris', 'Michelle'], 
+            2002: ['Isidore', 'Lili'], 
+            2003: ['Fabian', 'Isabel', 'Juan'], 
+            2004: ['Charley', 'Frances', 'Ivan', 'Jeanne'], 
+            2005: ['Dennis', 'Katrina', 'Rita', 'Stan', 'Wilma'], 
+            2007: ['Dean', 'Felix', 'Noel'], 
+            2008: ['Gustav', 'Ike', 'Paloma'], 
+            2010: ['Igor', 'Tomas'], 
+            2011: ['Irene'], 
+            2012: ['Sandy'], 
+            2013: ['Ingrid'], 
+            2015: ['Erika', 'Joaquin'], 
+            2016: ['Matthew', 'Otto'], 
+            2017: ['Harvey', 'Irma', 'Maria', 'Nate']
+           }
+
+
+# In[6]:
+
+Pacific = {
+    #1965: ['Hazel'], #Retired for unknown reasons
+    #1970: ['Adele'], #Retired for unknown reasons
+    1978: ['Fico'], 
+    1982: ['Iwa'], 
+    #1987: ['Knut'], #Retired for unknown reasons
+    #1988: ['Iva'], #Retired due to similarity to Iwa
+    1991: ['Fefa'], 
+    1992: ['Iniki'],
+    1995: ['Ismael'], 
+    1997: ['Pauline', 'Paka'], 
+    #2001: ['Adolph'], #Retired for political reasons
+    2002: ['Kenna'], 
+    #2004: ['Isis'], #Retired for political reasons
+    2006: ['Ioke'],
+    2008: ['Alma'], 
+    2013: ['Manuel'], 
+    2014: ['Odile'], 
+    2015: ['Patricia']
+}
+
+
+# In[ ]:
+
+
+
